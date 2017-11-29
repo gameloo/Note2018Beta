f7a0c6d9b01dabf73f9ad37ee477f3903d960ebd
gameloo <gameloo@mail.ru>
2017-11-29 12:24:46 +0300

    fix: del all notes in new Thread

--- Note2018/Note2018/pageSettings.xaml.cs
+++ Note2018/Note2018/pageSettings.xaml.cs
@@ -9,33 +9,58 @@ using Xamarin.Forms.Xaml;
 
 namespace Note2018
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class pageSettings : ContentPage
-	{
-		public pageSettings ()
-		{
-			InitializeComponent ();
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class pageSettings : ContentPage
+    {
+        public pageSettings()
+        {
+            InitializeComponent();
 
             var PageItems = new List<Label>();
 
             PageItems.Add(new Label { Text = "Удалить все заметки", ClassId = "ClearDB" });
+            PageItems.Add(new Label { Text = "Добавить 100 заметок", ClassId = "Add100Note" });
 
             settingsListView.ItemsSource = PageItems;
-		}
+        }
 
-        private async void settingsItemSelected (object sender, SelectedItemChangedEventArgs e)
+        private async void settingsItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             Label selectLabel = (Label)e.SelectedItem;
+            object locker = new object();
+
             switch (selectLabel.ClassId)
             {
                 case "ClearDB":
                     {
-                        foreach(Note note in App.database.GetItems(NoteRepository.RequestItems.AllItems)) // Проверить работоспособность
-                        App.Database.DeleteItem(note.Id);
+                        Task task = new Task(() =>
+                        {
+                            foreach (Note note in App.database.GetItems(NoteRepository.RequestItems.AllItems)) // Проверить работоспособность
+                                App.Database.DeleteItem(note.Id);
+                            Device.BeginInvokeOnMainThread(async() => {await DisplayAlert("Уведомление", "Все заметки были удалены", "OK"); });
+                        });
+
+                        bool result = await DisplayAlert("Подтвердить действие", "Вы хотите удалить все заметки?", "Да", "Нет");
+                        lock (locker) if (result == true) task.Start();
+                        break;
+                    }
+                case "Add100Note":
+                    {
+                        Task task = new Task(() =>
+                        {
+                            for (int i = 0; i < 1000; i++)
+                            {
+                                Note note = new Note();
+                                note.Header = "TestNote" + i.ToString();
+                                App.Database.SaveItem(note);
+                            }
+                            Device.BeginInvokeOnMainThread(async() => {await DisplayAlert("Уведомление", "Создано 1000 записей", "OK"); });
+                        });
+
+                        lock (locker) task.Start();
                         break;
                     }
             }
         }
-
     }
 }
