15838e51bd4ad5680d3df0ec386bb6a434a6ab99
gameloo <gameloo@mail.ru>
2017-11-30 12:55:44 +0300

    Add localize class

---
+++ Note2018/Note2018.Android/Localize_Android.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Note2018.Droid;
+
+[assembly:Dependency(typeof(Localize_Android))]
+namespace Note2018.Droid
+{
+    public class Localize_Android:ILocalize
+    {
+        public System.Globalization.CultureInfo GetCurrentCultureInfo()
+        {
+            var androidLocale = Java.Util.Locale.Default;
+            var netLanguage = androidLocale.ToString().Replace("_", "-");
+            return new System.Globalization.CultureInfo(netLanguage);
+        }
+    }
+}
---
+++ Note2018/Note2018/TranslateExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Resources;
+using System.Globalization;
+using System.Reflection;
+
+namespace Note2018
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        readonly CultureInfo cultureInfo;
+        const string ResourceId = "Note2018.Resource";
+
+        public TranslateExtension()
+        {
+            cultureInfo = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+        }
+
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null) return "";
+
+            ResourceManager resourceManager = new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly);
+
+            var translation = resourceManager.GetString(Text, cultureInfo);
+
+            if (translation == null)
+            {
+                translation = Text;
+            }
+            return translation;
+        }
+    }
+}
